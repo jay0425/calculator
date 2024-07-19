515f161d71214e4a04ac2238a4fc2ed4a598416d
jay0425 <heedev94@gmail.com>
2024-07-19 22:18:16 +0900

    연산자 input 박스에 나오게 구현

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 let display = document.getElementById('display');
 let currentInput = '';
 
-function appendNumber(number) {
+const appendNumber = (number) => {
   // input 박스에 입력한 숫자와 연산자들을 넣는 함수
   currentInput += number.toString();
   display.value = currentInput;
-}
+};
 
-function clearDisplay() {
+const clearDisplay = () => {
   currentInput = '';
   display.value = currentInput;
-}
+};
+
+const appendOperation = (operation) => {
+  currentInput += operation;
+  display.value = currentInput;
+};
