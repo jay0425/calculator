b0bacc9ece70cba42abd7c289b51df609848f913
jay0425 <heedev94@gmail.com>
2024-07-19 22:11:40 +0900

    숫자 input 창에 뜨게 하기, c 클릭 시 모든 숫자 제거 구현

--- script.js
+++ script.js
@@ -2,34 +2,12 @@ let display = document.getElementById('display');
 let currentInput = '';
 
 function appendNumber(number) {
+  // input 박스에 입력한 숫자와 연산자들을 넣는 함수
   currentInput += number.toString();
-  updateDisplay();
-}
-
-function appendOperation(operation) {
-  const lastChar = currentInput.slice(-1);
-  if (['+', '-', '*', '/'].includes(lastChar)) {
-    currentInput = currentInput.slice(0, -1) + operation;
-  } else {
-    currentInput += operation;
-  }
-  updateDisplay();
-}
-
-function calculateResult() {
-  try {
-    currentInput = eval(currentInput).toString();
-  } catch (e) {
-    currentInput = 'Error';
-  }
-  updateDisplay();
+  display.value = currentInput;
 }
 
 function clearDisplay() {
   currentInput = '';
-  updateDisplay();
-}
-
-function updateDisplay() {
   display.value = currentInput;
 }
